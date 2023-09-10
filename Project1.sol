87ff49241af0b813be091420ab787ae076598700
PiyushCh67 <129509600+PiyushCh67@users.noreply.github.com>
2023-09-10 23:53:32 +0530

    Update Project1.sol

--- Project1.sol
+++ Project1.sol
@@ -1,19 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract MetacrafterModule1 {
+contract MetacrafterModule1 
+{
     
-    function clubEntry(uint age ) public {
+    function clubEntry(uint age ) public 
+    {
 
         require(ag >= 10, "Person should not be minor ");
-}
+    }
 
-    function Division(int first_num, int second_num) public pure returns (int) {
+    function Division(int first_num, int second_num) public pure returns (int) 
+    {
         assert( second_num != 0 ); // Denominator can not be zero otherwise it would be infinity and error will occur 
         return first_num / second_num ;
     }
 
-    function Kabaddi(uint Players) public {
+    function Kabaddi(uint Players) public 
+    {
         if (Players > 7) {
             revert("Players can't be more than 7 ");
     }
